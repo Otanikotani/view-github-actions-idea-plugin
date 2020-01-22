b28d2998ba16b24311edd056a62770c9b24120f5
Andrey Artyukhov <otanikotani@gmail.com>
2020-01-22 14:17:12 -0500

    Not much yet

--- src/main/java/org/github/otanikotani/Library.java
+++ src/main/java/org/github/otanikotani/Library.java
@@ -1,7 +1,17 @@
 package org.github.otanikotani;
 
+import org.kohsuke.github.GHOrganization;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 public class Library {
     public boolean someLibraryMethod() {
         return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        GitHub github = GitHub.connect();
+        GHOrganization org = github.getOrganization("trilogy-group");
+        GHRepository repo = org.getRepository("zAAAA");
     }
 }
