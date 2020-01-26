b8ef1ec7fb806c11c8bab9108a5446d21d525279
Tofik Mamishov <mamishov.tofik@gmail.com>
2020-01-26 16:13:00 +0300

    Removed token.

--- checks-plugin/src/main/java/org/github/otanikotani/dto/RepoDetailsDto.java
+++ checks-plugin/src/main/java/org/github/otanikotani/dto/RepoDetailsDto.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RepoDetailsDto {
-    private String token = "9a31338e81d6a20b718dece7b4b6aaf267b95968";
+    private String token = "token";
     private String owner = "otanikotani";
     private String repo = "view-github-actions-idea-plugin";
     private String branch = "heads/feature/mvp";
