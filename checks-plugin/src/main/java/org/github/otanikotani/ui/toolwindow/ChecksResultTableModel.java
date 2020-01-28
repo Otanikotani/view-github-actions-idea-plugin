31658d094c24f55415ec5381e96070cebd363233
Tofik Mamishov <mamishov.tofik@gmail.com>
2020-01-28 21:02:51 +0300

    Results are shown in a table.

---
+++ checks-plugin/src/main/java/org/github/otanikotani/ui/toolwindow/ChecksResultTableModel.java
@@ -0,0 +1,30 @@
+package org.github.otanikotani.ui.toolwindow;
+
+import javax.swing.table.DefaultTableModel;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+public class ChecksResultTableModel extends DefaultTableModel {
+
+    private static final List<String> COLUMN_NAMES = Arrays.asList("Name", "Status", "Started At");
+
+    public ChecksResultTableModel() {
+        columnIdentifiers.addAll(COLUMN_NAMES);
+    }
+
+    public void addRow(String name, String status, String startedAt) {
+        addRow(new Vector<>(Arrays.asList(name, status, startedAt)));
+    }
+
+    public void removeAllRows() {
+        while (getRowCount() > 0) {
+            removeRow(getRowCount() - 1);
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        return false;
+    }
+}
--- checks-plugin/src/main/java/org/github/otanikotani/ui/toolwindow/ChecksToolWindowContent.java
+++ checks-plugin/src/main/java/org/github/otanikotani/ui/toolwindow/ChecksToolWindowContent.java
@@ -1,59 +1,32 @@
 package org.github.otanikotani.ui.toolwindow;
 
+import com.intellij.ui.table.JBTable;
 import lombok.Getter;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import java.awt.Component;
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 public class ChecksToolWindowContent extends JPanel {
-    private static final GridLayout MAIN_LAYOUT = new GridLayout(1, 3);
+
+    private ChecksResultTableModel tableModel;
 
     public ChecksToolWindowContent() {
-        super(MAIN_LAYOUT);
-        MAIN_LAYOUT.setHgap(10);
-        MAIN_LAYOUT.setVgap(5);
-
-        JLabel nameLabel = new JLabel("Name");
-        add(nameLabel);
-        JLabel statusLabel = new JLabel("Status");
-        statusLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        add(statusLabel);
-        JLabel startedAtLabel = new JLabel("Started At");
-        add(startedAtLabel);
+        super(new GridLayout(1, 0));
+        tableModel = new ChecksResultTableModel();
+        final JBTable table = new JBTable(tableModel);
+        table.setRowSelectionAllowed(false);
+        setLayout(new BorderLayout());
+        add(table.getTableHeader(), BorderLayout.PAGE_START);
+        add(table, BorderLayout.CENTER);
     }
 
     public void addRow(String name, String status, String startedAt) {
-        MAIN_LAYOUT.setRows(MAIN_LAYOUT.getRows() + 1);
-        MAIN_LAYOUT.setColumns(3);
-
-        add(new JLabel(name));
-        JLabel statusLabel = new JLabel(status);
-        statusLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        add(statusLabel);
-        add(new JLabel(startedAt));
-
-        revalidate();
-        repaint();
+        tableModel.addRow(name, status, startedAt);
     }
 
     public void removeAllRows() {
-        List<Component> toDelete = new ArrayList<>();
-        for (int i = 1; i < MAIN_LAYOUT.getRows(); i++) {
-            toDelete.add(getComponent(i * MAIN_LAYOUT.getColumns()));
-            toDelete.add(getComponent(i * MAIN_LAYOUT.getColumns() + 1));
-            toDelete.add(getComponent(i * MAIN_LAYOUT.getColumns() + 2));
-        }
-        toDelete.forEach(this::remove);
-
-        MAIN_LAYOUT.setRows(1);
-        MAIN_LAYOUT.setColumns(3);
-        revalidate();
-        repaint();
+        tableModel.removeAllRows();
     }
 }
