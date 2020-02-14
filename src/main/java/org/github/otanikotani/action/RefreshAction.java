5e73bafa4609a481274b3d2762bfdede818c808d
Tofik Mamishov <mamishov.tofik@gmail.com>
2020-02-14 19:19:53 +0300

    Renamed "Refresh" action to "Refresh Checks".

--- src/main/java/org/github/otanikotani/action/RefreshAction.java
+++ src/main/java/org/github/otanikotani/action/RefreshAction.java
@@ -10,7 +10,7 @@ public class RefreshAction extends AnAction {
     private Runnable refreshChecksPanel;
 
     public RefreshAction(@NotNull Runnable refreshChecksPanel) {
-        super("Refresh", "Refreshes checks", AllIcons.Actions.Refresh);
+        super("Refresh Checks", "Refreshes checks", AllIcons.Actions.Refresh);
         this.refreshChecksPanel = refreshChecksPanel;
     }
 
--- src/test/groovy/org/github/otanikotani/action/RefreshActionSpec.groovy
+++ src/test/groovy/org/github/otanikotani/action/RefreshActionSpec.groovy
@@ -8,7 +8,7 @@ class RefreshActionSpec extends Specification {
   def "refresh action has proper text and description"() {
     expect:
     def action = new RefreshAction({})
-    action.templatePresentation.text == 'Refresh'
+    action.templatePresentation.text == 'Refresh Checks'
     action.templatePresentation.description == 'Refreshes checks'
   }
 
